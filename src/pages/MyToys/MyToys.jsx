6753b181da84a0087b092c9300babf51e85543d1
jahirul94 <jahirulislam41185@gmail.com>
2023-05-20 02:57:05 +0600

    added sort bye price section on my toys page

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -8,9 +8,10 @@ const MyToys = () => {
     const { user } = useContext(AuthContext);
     const [reload, setReload] = useState(false)
     const [myToys, setMyToys] = useState([]);
-    const url = `http://localhost:5000/myToys?email=${user?.email}`;
+    const [ sort , setSort ] = useState("")
+    const url = `http://localhost:5000/myToys?email=${user?.email}&sort=${sort}`;
 
-//    load my data 
+    //    load my data 
     useEffect(() => {
         fetch(url, {
             method: 'GET'
@@ -21,7 +22,7 @@ const MyToys = () => {
             })
 
     }, [url, reload])
- 
+
     // delete functionality
     const handleDelete = (id) => {
         Swal.fire({
@@ -50,14 +51,27 @@ const MyToys = () => {
                         }
                     })
                 // end 
-                
             }
         })
     }
-
+ console.log(sort);
 
     return (
         <div className="my-10 min-h-screen">
+            <div className="text-end px-10">
+                {/* dropdown start  */}
+                   <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost rounded-btn">Sort By</label>
+                        <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            <li><button onClick={() => setSort("")}>Default</button></li>
+                            <li><button onClick={() => setSort(1)}>Price {"("} Low {">"} High {")"}</button></li>
+                            <li><button onClick={ () => setSort(-1)}>Price {"("} High {">"} Low {")"} </button></li>
+                        </ul>
+                    </div>
+                {/* end */}
+
+            </div>
+            <div className="divider"></div>
             {
                 myToys.map(myToy => <DisplayMyToys
                     key={myToy._id}
@@ -71,3 +85,13 @@ const MyToys = () => {
 };
 
 export default MyToys;
+
+
+{/* <button
+className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+>
+<span>Dropdown</span>
+<svg className="fill-current h-4 w-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+    <path d="M7 10l5 5 5-5H7z" />
+</svg>
+</button> */}
